2e09ef7190926cf6f81ca3ca0fc454f7df4275e2
Florian Sabani <florian.sabani@generali.com>
2021-12-11 18:37:00 +0100

    Now simulator can take kid id as parameter

--- bot_simulator.js
+++ bot_simulator.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var argv = require('minimist')(process.argv.slice(2));
 
 var headers = {
   'Authorization': 'Bearer 8f7f4d105a040fd3dc3ebe5188657221b9f17915fc66c962ec6444f2ca9af3953a160353b427c7b766b90f9826a2f15f35394adb8883c58ef0aa07f357d9bb876421fbe9a95b45e025aa8c76ab33514d2b91b306968e830a10c22297d7220e5aef25d9f2ee66eb1026688762256f7945a5a1dd54047a87df652bf72c6e87cb52d05090ed55df2f5e1d9812ef5be9bd853a5fb2a0a337048000d915d09e68cc481ecbd45ae44b434f5e132c97d0c94f0ec6ed782c4523309f6eda6fbfaa2736e6',
@@ -9,9 +10,12 @@ let startX = 45.50277703374562;
 let startY = 12.261095921498931;
 let step =    0.0001;
 
+
+const BIMBO_ID =argv.u;
+
 async function simulate() {
 
-  var dataString = 'who=bimbo1&lat=' +
+  var dataString = 'who=' + BIMBO_ID + '&lat=' +
     startX
     + '&long=' +
     startY + '';
